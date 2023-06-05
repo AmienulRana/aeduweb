fc180fd8b3053a96264327bf3e06c40a82b37b04
AmienulRana <amienulrana@gmail.com>
2023-06-05 14:33:56 +0700

    create responsive website

--- src/components/layout/auth.tsx
+++ src/components/layout/auth.tsx
@@ -15,7 +15,7 @@ export default function AuthLayout({
 }: AuthLayoutProp) {
   return (
     <div className="flex min-h-screen">
-      <div className="w-1/4 py-6 max-w-[600px] text-white min-h-screen px-6 bg-primary">
+      <div className="w-1/4 hidden md:block py-6 max-w-[600px] text-white min-h-screen px-6 bg-primary">
         <p className="text-sm font-semibold">AEDU</p>
         <div className="translate-y-[100px]">
           <h1 className="text-3xl">Start your professional journey with us.</h1>
@@ -26,7 +26,7 @@ export default function AuthLayout({
         </div>
       </div>
       <div className="flex-1 flex justify-center items-center">
-        <div className="max-w-[1200px]">
+        <div className="min-w-[80%] md:min-w-max max-w-[1200px]">
           <p className="font-medium text-lg">{title}</p>
           <p className="text-gray-400 text-sm mb-5">{subTitle}</p>
           {oAuth && (
