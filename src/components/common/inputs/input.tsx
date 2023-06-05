089be475a0f2809f8046e72b22e8739c66219b8e
AmienulRana <amienulrana@gmail.com>
2023-06-05 10:05:28 +0700

    create input reusable icon

---
+++ src/components/common/inputs/input.tsx
@@ -0,0 +1,45 @@
+import { FC, HTMLAttributes } from "react";
+
+interface InputProps extends HTMLAttributes<HTMLInputElement> {
+  label?: string;
+  placeholder?: string;
+  type?: string;
+  className?: string;
+  value: any;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  pattern?: string;
+}
+
+const Input: FC<InputProps> = (props: InputProps) => {
+  const {
+    label,
+    placeholder,
+    type = "text",
+    className = "",
+    value,
+    onChange = () => {},
+    ...restProps
+  } = props;
+
+  const concatClassName = [
+    className,
+    "w-full border border-solid border-secondary py-3 px-4 rounded text-xs bg-secondary focus:bg-white outline-0",
+  ].join(" ");
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {label && <p className="font-semibold mt-5 mb-4">{label}</p>}
+      <input
+        className={concatClassName}
+        placeholder={placeholder}
+        required
+        value={value}
+        onChange={onChange}
+        type={type}
+        {...restProps}
+      />
+    </div>
+  );
+};
+
+export default Input;
