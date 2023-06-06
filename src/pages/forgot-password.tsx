665faa25663cf50ffef808717ca541a821a9e8ec
AmienulRana <amienulrana@gmail.com>
2023-06-06 10:50:02 +0700

    create validation in forgot password

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -6,9 +6,21 @@ import { FiMail } from "react-icons/fi";
 
 export default function Home() {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const handleShowPassword = () => setShowPassword(!showPassword);
+  const [isValidEmail, setIsValidEmail] = useState(true);
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputEmail = event.target.value;
+    setEmail(inputEmail);
+    if (inputEmail !== "") {
+      setIsValidEmail(validateEmail(inputEmail));
+    } else {
+      setIsValidEmail(true);
+    }
+  };
+  const validateEmail = (email: string) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|id|co\.id|io|co)$/;
+    return regex.test(email);
+  };
   return (
     <AuthLayout
       oAuth={false}
@@ -18,14 +30,22 @@ export default function Home() {
       <div className="md:w-[450px] px-4">
         <div className="relative">
           <Input
-            label="Enter your email"
-            placeholder="unames@gmail.com"
+            label="Email"
+            placeholder="Input your email here"
             className="pl-10"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleEmailChange(e)
+            }
           />
           <FiMail className="text-gray-400 absolute bottom-3.5 left-4" />
         </div>
+        {!isValidEmail && (
+          <p className="text-red-500 text-xs mt-2 mb-3">
+            Please enter a valid email with allowed domains such as .com, .id,
+            .co.id, or .io
+          </p>
+        )}
         <div className="flex mt-1.5 justify-end">
           <Link
             href="/"
@@ -34,9 +54,18 @@ export default function Home() {
             Back to login
           </Link>
         </div>
-        <button className="text-center font-bold text-white w-full mt-6 rounded-md duration-100 hover:opacity-80 bg-primary py-3 px-4">
-          Reset Password
-        </button>
+        {isValidEmail && email ? (
+          <button className="text-center font-bold text-white w-full mt-6 rounded-md duration-100 hover:opacity-80 bg-primary py-3 px-4">
+            Reset Password
+          </button>
+        ) : (
+          <button
+            disabled
+            className="text-center opacity-70 font-bold text-white w-full mt-6 rounded-md duration-100 bg-primary py-3 px-4"
+          >
+            Reset Password
+          </button>
+        )}
         <p className="text-gray-400 mt-3">
           Don&apos;t have a account?{" "}
           <Link href="/sign-up" className="text-primary underline">
