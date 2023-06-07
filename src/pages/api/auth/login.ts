06b9a04db9e67946909cf2d107bef9074bc512e0
AmienulRana <amienulrana@gmail.com>
2023-06-07 21:50:32 +0700

    integration to api login

---
+++ src/pages/api/auth/login.ts
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { URL_API } from "@/config";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      const { email, password } = req.body;
+      const response = await axios.post(`${URL_API}/login`, {
+        email,
+        password,
+      });
+      if (response?.data?.msg === "Incorrect Email or Password") {
+        throw Error(response?.data?.msg);
+      }
+      res.status(200).json(response?.data);
+    } catch (error: any) {
+      res.status(422).json({ message: "Incorrect Email or Password" });
+      console.log(error);
+    }
+  }
+  res.status(200).json({ name: "John Doe" });
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,18 @@ import Link from "next/link";
 import { useState } from "react";
 import { FiMail, FiLock, FiEyeOff, FiEye } from "react-icons/fi";
 import { TYPOGRAPHY } from "@/data/typhography";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { URL_LEARNING_AEDU } from "@/config";
 
 export default function Home() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [isValidEmail, setIsValidEmail] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
 
   const handleShowPassword = () => setShowPassword(!showPassword);
 
@@ -27,6 +33,27 @@ export default function Home() {
   const validateEmail = (email: string) => {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|id|co\.id|io|co)$/;
     return regex.test(email);
+  };
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      const payload = {
+        email,
+        password,
+      };
+      const response = await axios.post(`/api/auth/login`, { ...payload });
+      // router.push("/");
+      window.location.href = URL_LEARNING_AEDU;
+      // console.log(response);
+      setIsLoading(false);
+    } catch (error: any) {
+      setErrorMessage(
+        error?.response?.data?.message || "Failed to authentication"
+      );
+      setIsLoading(false);
+    }
   };
   return (
     <AuthLayout title="Sign in" subTitle="Login to your account">
@@ -79,22 +106,25 @@ export default function Home() {
           </Link>
         </div>
         {isValidEmail && email && password ? (
-          <Link
-            href="https://portal-aedu.vercel.app/"
-            target="_blank"
-            rel="noreferrer"
+          <button
+            className={`text-center font-bold text-white w-full mt-6 rounded-md duration-100 hover:opacity-70 bg-primary py-3 px-4 ${
+              isLoading && "opacity-70"
+            }`}
+            disabled={isLoading}
+            onClick={() => handleSubmit()}
           >
-            <button className="text-center font-bold text-white w-full mt-6 rounded-md duration-100 hover:opacity-80 bg-primary py-3 px-4">
-              {TYPOGRAPHY.SIGN_UP}
-            </button>
-          </Link>
+            {TYPOGRAPHY.SIGN_IN}
+          </button>
         ) : (
           <button
             disabled
             className="text-center opacity-70 font-bold text-white w-full mt-6 rounded-md duration-100 bg-primary py-3 px-4"
           >
-            {TYPOGRAPHY.SIGN_UP}
+            {TYPOGRAPHY.SIGN_IN}
           </button>
+        )}
+        {errorMessage && (
+          <p className="text-red-500 text-xs mt-2 mb-3">{errorMessage}!</p>
         )}
         <p className="text-gray-400 mt-3">
           {TYPOGRAPHY.NOT_HAVE_ACCOUNT}
