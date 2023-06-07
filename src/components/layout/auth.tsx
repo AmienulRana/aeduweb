9d99646eef005ade7f5c0d2c23ae42ab36d78ae5
AmienulRana <amienulrana@gmail.com>
2023-06-07 11:28:44 +0700

    create responsive in sidebar

--- src/components/layout/auth.tsx
+++ src/components/layout/auth.tsx
@@ -21,30 +21,35 @@ export default function AuthLayout({
   }
   return (
     <div className="flex min-h-screen">
-      <div className="w-[30%] hidden md:block py-6 max-w-[600px] text-white min-h-screen px-6 bg-primary">
+      <div className="lg:w-[30%] md:w-[35%] relative md:block hidden justify-center py-6 max-w-[600px] text-white min-h-screen px-6 bg-primary">
         <Image
           src="/aedu-white.svg"
           alt="logo aedu white"
           width={80}
           height={80}
+          className="absolute top-5 left-4"
         />
-        <div className="mt-[60px] mb-10">
-          <h1 className="text-4xl">Start your professional journey with us.</h1>
-          <p className="text-sm mt-5">
-            discover tailored made courses and community of experts and learners
-            to expand your learning experience
-          </p>
-        </div>
+        <div className="md:flex flex-col justify-center h-full">
+          <div className="lg:mt-[60px] mb-10">
+            <h1 className="text-4xl">
+              Start your professional journey with us.
+            </h1>
+            <p className="text-sm mt-5">
+              discover tailored made courses and community of experts and
+              learners to expand your learning experience
+            </p>
+          </div>
 
-        <div className="relative">
-          <Image
-            src="/highlight.png"
-            height={215}
-            width={215}
-            alt="highlight"
-            style={{ width: "100%" }}
-          />
-          <span className="block absolute opacity-60 top-0 left-0 w-full h-full bg-primary" />
+          <div className="relative">
+            <Image
+              src="/highlight.png"
+              height={215}
+              width={215}
+              alt="highlight"
+              style={{ width: "100%" }}
+            />
+            <span className="block absolute opacity-60 top-0 left-0 w-full h-full bg-primary" />
+          </div>
         </div>
       </div>
       <div className="flex-1 flex justify-center items-center">
