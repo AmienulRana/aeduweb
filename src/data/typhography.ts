22f85a3d3eb9f53332ac8dcde904615825cf4b08
AmienulRana <amienulrana@gmail.com>
2023-06-07 12:07:46 +0700

    add typhography for layout auth

--- src/data/typhography.ts
+++ src/data/typhography.ts
@@ -1,5 +1,10 @@
-const TYPOGRAPHY = {
+export const TYPOGRAPHY = {
   SIGN_IN: "Sign in",
   SIGN_UP: "Sign up",
-  FORGOT_PASSWORD: "Forgot Password",
+  FORGOT_PASSWORD: "Forgot Password?",
+  INTRODUCTION_TITLE: "Start your professional journey with us.",
+  INTRODUCTION_DESC:
+    "discover tailored made courses and community of experts and  learners to expand your learning experience",
+  GOOGLE_SIGNIN: "Continue with goggle",
+  OR: "OR",
 };
