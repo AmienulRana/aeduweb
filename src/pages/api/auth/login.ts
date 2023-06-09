da9fa0f46a57da9159bf0f9b2511c2317ff796b8
AmienulRana <amienulrana@gmail.com>
2023-06-09 13:45:31 +0700

    set cookies when user success login

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 import { URL_API } from "@/config";
+import cookie from "cookie";
 
 type Data = {
   name: string;
@@ -18,12 +19,27 @@ export default async function handler(
         email,
         password,
       });
+      if (
+        response?.status == 200 &&
+        response?.data?.msg !== "Incorrect Email or Password"
+      ) {
+        res.setHeader(
+          "Set-Cookie",
+          cookie.serialize("jwtToken", response?.data?.token, {
+            domain: ".aedu.id",
+            path: "/",
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 1,
+          })
+        );
+        return res.status(200).json(response?.data);
+      }
       if (response?.data?.msg === "Incorrect Email or Password") {
         throw Error(response?.data?.msg);
       }
-      res.status(200).json(response?.data);
+      throw Error();
     } catch (error: any) {
-      res.status(422).json({ message: "Incorrect Email or Password" });
+      return res.status(422).json({ message: "Incorrect Email or Password" });
     }
   }
   res.status(200).json({ name: "John Doe" });
