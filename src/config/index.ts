0457fc84adb40f6d34eab03480ea8ef67e4c94b8
AmienulRana <amienulrana@gmail.com>
2023-06-12 21:01:11 +0700

    change mode to production

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const URL_API = "https://api.aedu.id";
-export const MODE = "dev";
+export const MODE: string = "prod";
 export const URL_LEARNING_AEDU =
   MODE === "dev" ? "http://localhost:3001" : "https://learning.aedu.id";
