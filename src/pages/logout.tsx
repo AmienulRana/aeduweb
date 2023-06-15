9b47852a40a62f66c19b9ae56f3a919ae8771e6a
AmienulRana <amienulrana@gmail.com>
2023-06-15 15:58:39 +0700

    change redirect to login page

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,3 +1,4 @@
+import { MODE } from "@/config";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -5,7 +6,11 @@ import { useEffect } from "react";
 export default function Logout() {
   const router = useRouter();
   useEffect(() => {
-    signOut().then(() => router.push("/"));
+    signOut().then(
+      () =>
+        (window.location.href =
+          MODE === "dev" ? "http://localhost:3001" : "https://learning.aedu.id")
+    );
   }, []);
   return null;
 }
