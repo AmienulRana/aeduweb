59951f7f83c8d5032f493b036a77dbe1b9167dc6
AmienulRana <amienulrana@gmail.com>
2023-06-15 22:34:58 +0700

    remove authorization url

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,8 +5,6 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization:
-        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
     }),
   ],
   secret: process.env.JWT_SECRET,
