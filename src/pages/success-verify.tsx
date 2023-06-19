41e3589f8dd4f709aee33a9ae0b29d30398d9461
AmienulRana <amienulrana@gmail.com>
2023-06-19 10:34:34 +0700

    runv function handleVerivication for validation email

--- src/pages/success-verify.tsx
+++ src/pages/success-verify.tsx
@@ -1,4 +1,6 @@
 import { AuthLayout } from "@/components/layout";
+import { URL_API } from "@/config";
+import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -6,14 +8,32 @@ import { useEffect, useState } from "react";
 export default function VerifySuccess() {
   const [time, setTime] = useState(4);
   const router = useRouter();
+  const handleVerifyEmail = async (token: any) => {
+    try {
+      const response = await axios.post(`${URL_API}/verify/${token}`);
+      if (response.status === 200) {
+        // setTimeout(() => {
+        //   router.push("/");
+        // }, 3000);
+      }
+      // setIsLoading(false);
+    } catch (error: any) {
+      console.log(error);
+      // setError(error?.response?.data || "Failed to register account");
+      // setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
+    console.log(router.query.token);
+    handleVerifyEmail(router?.query?.token);
+
     setInterval(() => {
       setTime(time !== 0 ? time - 1 : 0);
-    }, 1000);
+    }, 5000);
     setTimeout(() => {
       router.push("/");
-    }, 3000);
+    }, 5000);
   }, []);
   return (
     <AuthLayout oAuth={false}>
