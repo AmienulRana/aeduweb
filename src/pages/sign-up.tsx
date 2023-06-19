5f9ed2de222e19bfeaf0ec38ac5cbff20cf3fd3a
AmienulRana <amienulrana@gmail.com>
2023-06-19 12:46:58 +0700

    create condition to notice user to check email'

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "next/font/google";
 import { AuthLayout } from "@/components/layout";
 import { Input } from "@/components/common/inputs";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiMail, FiLock, FiEyeOff, FiEye } from "react-icons/fi";
 import axios from "axios";
 axios.defaults.withCredentials = true;
@@ -46,7 +46,7 @@ export default function Home() {
   const handleSubmit = async () => {
     setIsLoading(true);
     setError("");
-    setSuccess(false);
+    setSuccess(true);
     try {
       const payload = {
         email,
@@ -57,8 +57,6 @@ export default function Home() {
       if (response.status === 200) {
         setSuccess(true);
         resetForm();
-        // handleVerifyEmail(response.data.token);
-        // router.push("/");
       }
       // console.log(response);
       // setIsLoading(false);
@@ -68,131 +66,135 @@ export default function Home() {
       setIsLoading(false);
     }
   };
-  const handleVerifyEmail = async (token: string) => {
-    try {
-      const response = await axios.post(`${URL_API}/verify/${token}`);
-      if (response.status === 200) {
-        // setTimeout(() => {
-        //   router.push("/");
-        // }, 3000);
-      }
-      setIsLoading(false);
-    } catch (error: any) {
-      setError(error?.response?.data || "Failed to register account");
-      setIsLoading(false);
+  useEffect(() => {
+    if (success) {
+      setTimeout(() => {
+        router.push("/");
+      }, 5000);
     }
-  };
+  }, [success]);
 
   return (
-    <AuthLayout title="Sign up" subTitle="Let's get started with us">
-      <div className="md:w-[450px] md:px-4">
-        {success && (
-          <p className="text-center border border-primary rounded-md mt-3 text-sm text-primary py-2.5">
-            Please check{" "}
-            <a
-              href="https://gmail.com"
-              target="_blank"
-              rel="noreferrer"
-              className="underline"
-            >
-              your email
-            </a>{" "}
-            to verify your account.
-          </p>
-        )}
-        <div className="relative">
-          <Input
-            label="Email"
-            placeholder="Input your email here"
-            className="pl-10"
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              handleEmailChange(e)
-            }
-          />
-          <FiMail className="text-gray-400 absolute bottom-3.5 left-4" />
-        </div>
-        {!isValidEmail && (
-          <p className="text-red-500 text-xs px-4 mt-2">
-            Please enter a valid email with allowed domains such as .com, .id,
-            .co.id, or .io
-          </p>
-        )}
-        <div className="relative">
-          <Input
-            label="Password"
-            placeholder="Input your password here"
-            className="pl-10"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type={showPassword ? "text" : "password"}
-          />
-          {showPassword ? (
-            <FiEye
-              className="text-gray-400 cursor-pointer absolute bottom-3.5 right-4"
-              onClick={handleShowPassword}
-            />
-          ) : (
-            <FiEyeOff
-              className="text-gray-400 cursor-pointer absolute bottom-3.5 right-4"
-              onClick={handleShowPassword}
-            />
-          )}
-          <FiLock className="text-gray-400 absolute bottom-3.5 left-4" />
-        </div>
-        <div className="relative">
-          <Input
-            label="Confirm Password"
-            placeholder="Input your confirm password here"
-            type={showPassword ? "text" : "password"}
-            className="pl-10"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          {showPassword ? (
-            <FiEye
-              className="text-gray-400 cursor-pointer absolute bottom-3.5 right-4"
-              onClick={handleShowPassword}
-            />
-          ) : (
-            <FiEyeOff
-              className="text-gray-400 cursor-pointer absolute bottom-3.5 right-4"
-              onClick={handleShowPassword}
-            />
-          )}
-          <FiLock className="text-gray-400 absolute bottom-3.5 left-4" />
-        </div>
-        {confirmPassword && password !== confirmPassword && (
-          <p className="text-red-500 text-xs mt-2 mb-2">
-            Password and confirm password do not match
-          </p>
-        )}
-        {error && <p className="text-red-500 text-xs mt-2 mb-2">{error}</p>}
-        {isValidEmail && email && password && confirmPassword ? (
-          <button
-            className={`text-center font-bold text-white w-full mt-6 rounded-md duration-100 hover:opacity-80 bg-primary py-3 px-4 ${
-              isLoading && "opacity-70"
-            }`}
-            disabled={isLoading}
-            onClick={() => handleSubmit()}
-          >
-            Sign up
-          </button>
-        ) : (
-          <button
-            disabled
-            className="text-center opacity-70 font-bold text-white w-full mt-6 rounded-md duration-100 bg-primary py-3 px-4"
-          >
-            Sign up
-          </button>
-        )}
-        <p className="text-gray-400 mt-3">
-          Already have a account?{" "}
-          <Link href="/" className="text-primary underline">
-            Sign in
-          </Link>
-        </p>
-      </div>
-    </AuthLayout>
+    <>
+      {success ? (
+        <AuthLayout
+          title="Register Pending"
+          oAuth={false}
+          subTitle="Verify your account by checking your email"
+        >
+          <div className="md:w-[450px] md:px-4">
+            <p className="text-center border border-primary rounded-md mt-3 text-sm text-primary py-2.5">
+              Please check{" "}
+              <a
+                href="https://gmail.com"
+                target="_blank"
+                rel="noreferrer"
+                className="underline"
+              >
+                your email
+              </a>{" "}
+              to verify your account.
+            </p>
+          </div>
+        </AuthLayout>
+      ) : (
+        <AuthLayout title="Sign up" subTitle="Let's get started with us">
+          <div className="md:w-[450px] md:px-4">
+            <div className="relative">
+              <Input
+                label="Email"
+                placeholder="Input your email here"
+                className="pl-10"
+                value={email}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  handleEmailChange(e)
+                }
+              />
+              <FiMail className="text-gray-400 absolute bottom-3.5 left-4" />
+            </div>
+            {!isValidEmail && (
+              <p className="text-red-500 text-xs px-4 mt-2">
+                Please enter a valid email with allowed domains such as .com,
+                .id, .co.id, or .io
+              </p>
+            )}
+            <div className="relative">
+              <Input
+                label="Password"
+                placeholder="Input your password here"
+                className="pl-10"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type={showPassword ? "text" : "password"}
+              />
+              {showPassword ? (
+                <FiEye
+                  className="text-gray-400 cursor-pointer absolute bottom-3.5 right-4"
+                  onClick={handleShowPassword}
+                />
+              ) : (
+                <FiEyeOff
+                  className="text-gray-400 cursor-pointer absolute bottom-3.5 right-4"
+                  onClick={handleShowPassword}
+                />
+              )}
+              <FiLock className="text-gray-400 absolute bottom-3.5 left-4" />
+            </div>
+            <div className="relative">
+              <Input
+                label="Confirm Password"
+                placeholder="Input your confirm password here"
+                type={showPassword ? "text" : "password"}
+                className="pl-10"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              {showPassword ? (
+                <FiEye
+                  className="text-gray-400 cursor-pointer absolute bottom-3.5 right-4"
+                  onClick={handleShowPassword}
+                />
+              ) : (
+                <FiEyeOff
+                  className="text-gray-400 cursor-pointer absolute bottom-3.5 right-4"
+                  onClick={handleShowPassword}
+                />
+              )}
+              <FiLock className="text-gray-400 absolute bottom-3.5 left-4" />
+            </div>
+            {confirmPassword && password !== confirmPassword && (
+              <p className="text-red-500 text-xs mt-2 mb-2">
+                Password and confirm password do not match
+              </p>
+            )}
+            {error && <p className="text-red-500 text-xs mt-2 mb-2">{error}</p>}
+            {isValidEmail && email && password && confirmPassword ? (
+              <button
+                className={`text-center font-bold text-white w-full mt-6 rounded-md duration-100 hover:opacity-80 bg-primary py-3 px-4 ${
+                  isLoading && "opacity-70"
+                }`}
+                disabled={isLoading}
+                onClick={() => handleSubmit()}
+              >
+                Sign up
+              </button>
+            ) : (
+              <button
+                disabled
+                className="text-center opacity-70 font-bold text-white w-full mt-6 rounded-md duration-100 bg-primary py-3 px-4"
+              >
+                Sign up
+              </button>
+            )}
+            <p className="text-gray-400 mt-3">
+              Already have a account?{" "}
+              <Link href="/" className="text-primary underline">
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </AuthLayout>
+      )}
+    </>
   );
 }
